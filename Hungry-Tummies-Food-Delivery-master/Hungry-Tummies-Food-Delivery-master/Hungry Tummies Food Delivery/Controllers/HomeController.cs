fbcc283fc4234ac3044fb4020e7354d1b6e09157
Paulo NGOVE <paulon@bbd.co.za>
2022-05-15 17:47:12 +0200

    change frontend colors

---
+++ Hungry-Tummies-Food-Delivery-master/Hungry-Tummies-Food-Delivery-master/Hungry Tummies Food Delivery/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hungry_Tummies_Food_Delivery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Onboard()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [ActionName("successful-login")]
+        public IActionResult Success()
+        {
+            return View();
+        }
+        [ActionName("register-verify")]
+        public IActionResult Verify()
+        {
+            return View();
+        }
+        public IActionResult Location()
+        {
+            return View();
+        }
+    }
+}
