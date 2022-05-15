722ad06a3379f9737776e5fb4d61166744e6a108
Paulo NGOVE <paulon@bbd.co.za>
2022-05-15 17:48:47 +0200

    frontend changes

---
+++ Hungry-Tummies-Food-Delivery-master/Hungry-Tummies-Food-Delivery-master/Hungry Tummies Food Delivery/Views/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hungry_Tummies_Food_Delivery.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
